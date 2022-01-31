ec864e2e9fb8d5db24591090a17942d83ddcfb25
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-01-30 22:37:51 -0500

    Inicio proyecto Flappy bird En este proyecto se exportan los archivos bases que ha dado el profesor para comenzar a trabajar este trabajo de diagnostico

---
+++ main.js
@@ -0,0 +1,30 @@
+let canvas;
+
+function setup() {
+    frameRate(60);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style('z-index', '-1');
+    canvas.style('position', 'fixed');
+    canvas.style('top', '0');
+}
+
+function draw() {
+    //background(0, 50);
+    background(0);
+    newCursor();
+    
+}
+
+function mouseClicked(){
+    //bird.flyUpward();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function newCursor() {
+    noStroke();
+    fill(255);
+    ellipse(pmouseX, pmouseY, 10, 10);
+}
