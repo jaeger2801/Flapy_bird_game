3059039bfa07e37d23f9003e574d7907b24b26bd
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-02-03 22:54:02 -0500

    Crea movimiento del pajaro

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function setup() {
     canvas.style('z-index', '-1');
     canvas.style('position', 'fixed');
     canvas.style('top', '0');
-
+    canvas.style('right', '0');
     pajaro = new Pajaro();
 
 }
--- pajaro.js
+++ pajaro.js
@@ -1,21 +1,28 @@
 class Pajaro {
     
     constructor() {
-        this.y = 100
-        this.x = 100
+        this.y = height/2
+        this.x = width/2-150
 
         //movimiento
         this.gravedad = 1;
+        this.velocidad = 0;
         this.salto = 1;
     }
     
     pintarPajaro(){
         fill(255, 0, 0);
         ellipseMode(CENTER);
-        ellipse(this.x, this.y, 100, 100);
+        ellipse(this.x, this.y, 20, 20);
     }
 
     movimientoPajaro(){
-        this.y = this.gravedad ++;
+        this.velocidad += this.gravedad;
+        this.y += this.velocidad;
+
+        if(this.y > height) {
+            this.y = height;
+            this.gravedad = 0;
+        }
     }
 }
